345455e2c1f0f5304691acfd554ca9f4bf63de0a
Sola-Ade <solaadefarasin@hotmail.com>
2024-06-12 16:27:23 +0100

    lint

--- src/find-shortest-string.js
+++ src/find-shortest-string.js
@@ -11,7 +11,7 @@ function findShortestString(stringArr) {
   const shortestString = stringArr[0];
   for (let str of stringArr) {
     if (str.length < shortestString.length) {
-      shortestString = str;
+   /* const shortestString = str;*/
     }
   }
   return shortestString.length;
